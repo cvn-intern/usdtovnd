a52ea5677cd7ee6ee3cc3b428ebbb21239a4541f
vuuu0412 <vuuu0412@gmai.com>
2022-06-22 10:11:50 +0700

    test

--- src/App.test.js
+++ src/App.test.js
@@ -2,10 +2,10 @@ import { render, screen } from '@testing-library/react';
 import App from './App';
 
 test('renders learn react link', () => {
-  
+
   render(<App />);
   
-  const linkElement = screen.getByText(/Convert USD to TTT/i);
+  const linkElement = screen.getByText(/Convert USD to VND/i);
 
   expect(linkElement).toBeInTheDocument();
 
